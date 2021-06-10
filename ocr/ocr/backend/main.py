ddc53a1ef8bae9201b87e98b14e89cb6ce0d5d55
Xavier Perret <xa.perret@protonmail.ch>
2021-06-10 21:35:10 +0200

    fastapi can now serve static files

--- ocr/ocr/backend/main.py
+++ ocr/ocr/backend/main.py
@@ -1,34 +1,49 @@
 #!/usr/bin/env python3
+import os
+from typing import Dict, List, Optional, Set, Tuple
 
+import tensorflow as tf
 import uvicorn
-from typing import Optional, List, Set, Dict, Tuple
-from fastapi import FastAPI
-#import tensorflow as tf
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+from starlette.responses import FileResponse
+
 import image_recognition as ir
 
-app = FastAPI(title='TP de Vision Numerique')
-model = ir.load_model('model.json')
+app = FastAPI(title='TP de vision numérique')
 
 
-@app.get('/index')
+class Item(BaseModel):
+    content: List[List[int]]
+
+
+@app.get("/")
 async def read_root():
-    return "hello world"
+    return FileResponse('frontend/index.html')
+
+
+@app.get("/train")
+def read_item():
+    return {"hello": "world"}
+
+
+@app.put("/train")
+def update_item():
+    return {"hello": "world"}
 
 
 @app.post('/train')
-def train():
-    return "bim"
+async def train(item: Item):
+    print(item)
+    return item
 
 
 @app.post('/predict')
 def predict(data):
-    """ 
-
-    Params
-    data --
-    """
     return "bim"
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True)
+    app.mount("/frontend", StaticFiles(directory="frontend"), name="frontend")

b44f2db080fb698af7e24f3f035158afb3f9ac6a
Xavier Perret <xa.perret@protonmail.ch>
2021-06-12 22:58:34 +0200

    improved train function, need to do a function to convert labels into their right coding

--- ocr/ocr/backend/image_prediction.py
+++ ocr/ocr/backend/image_prediction.py
@@ -17,6 +17,11 @@ __author__ = "Xavier Perret"
 __email__ = "xavier.perret@etu.hesge.ch"
 __date__ = "12/06/2021"
 
+CANVAS_SIZE: int = 400
+NEURAL_NUMBER: int = 150
+CHAR_NUMBER: int = 10
+EPOCHS: int = 3
+
 
 def predict(image_list: List[int], model) -> List[List[float]]:
     """ Guess based on given matrix and model
@@ -28,25 +33,39 @@ def predict(image_list: List[int], model) -> List[List[float]]:
     return [[]]
 
 
-def get_features_labels(images_list: List[Tuple[str, List[int]]]) -> Tuple[List[str], List[int]]:
-    """ Return features and labels from given dict
+def get_features_labels(images_list: List[Tuple[str, List[int]]]) -> Tuple[List[str], List[List[int]]]:
+    """ Return features and labels from given list of tuples containing characters and their data
 
     Params
     images_list -- contains all images and their character they are representing
     """
-    features = [i[0] for i in images_list]
-    labels = [i[1] for i in images_list]
-    return features, labels
+    features = [i[1] for i in images_list]
+    labels = [i[0] for i in images_list]
+    return labels, features
 
 
-def train(features: List[str], labels: List[int], model) -> None:
-    """ Train and return current model with given dataset
+def convert_characters(labels: List[str]) -> List[int]:
+    """ Convert characters into the right coding
+    """
+    return []
+
+
+def train(labels: List[str], features: List[List[int]], model: tf.keras.Model) -> tf.keras.Model:
+    """ Create and train a model with given dataset and returns it
 
     Params
     image_list --
     model --
     """
-    new_model = tf.keras.Sequential()
-    new_model.compile()
+    print("train")
+    model.add(Dense(CANVAS_SIZE))
+    model.add(Dense(NEURAL_NUMBER, activation='relu'))
+    model.add(Dense(CHAR_NUMBER, activation='softmax'))
+    model.compile(
+        optimizer='rmsprop', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    print("  -> train summary")
+    model.summary()
+    model.fit(features, labels, epochs=EPOCHS,
+              batch_size=len(features))
 
-    return new_model
+    return model

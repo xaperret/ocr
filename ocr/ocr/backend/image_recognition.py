4a5eb1ccfb5c9115ed073039324d23b434718531
Xavier Perret <xa.perret@protonmail.ch>
2021-06-13 14:35:39 +0200

    removed useless print, changed image format writing/loading from json to csv

--- ocr/ocr/backend/image_recognition.py
+++ ocr/ocr/backend/image_recognition.py
@@ -3,6 +3,7 @@
 """
 
 import json
+import csv
 import os
 import shutil
 from typing import Dict, List, Optional, Set, Tuple
@@ -21,12 +22,13 @@ FILEPATH_MODELS: str = 'model'  # don't put precious folder @see delete_model()
 FILENAME_MODEL: str = 'model.json'
 STEP = 20
 CHARACTER_LIST = "0123456789"
+HEADER_ROW: List[str] = ["Character"] + [str(i) for i in range(0, 20)]
 
 
 def print_matrix(character: str, matrix: List[List[int]]) -> None:
     """ Print character and matrix
 
-    Params 
+    Params
     character -- character or symbol drawn on canvas
     matrix -- matrix representing the character
     """
@@ -62,7 +64,7 @@ def print_list_tuple(training_material: List[Tuple[str, List[int]]]) -> None:
 
 
 def matrix_2_list(matrix: List[List[int]]) -> List[int]:
-    """ Convert 2d matrix into one list and returns it 
+    """ Convert 2d matrix into one list and returns it
 
     Params
     matrix -- given 2d matrix to convert
@@ -107,9 +109,9 @@ def unload_model(model_to_unload, filepath: str = 'model') -> None:
 
 
 def load_images() -> List[Tuple[str, List[int]]]:
-    """ Return all datasets from datasets according to CHARACTER_LIST 
+    """ Return all datasets from datasets according to CHARACTER_LIST
 
-    the name of a file in datasets will be 'a.json' where a is 
+    the name of a file in datasets will be 'a.json' where a is
     the character of the given symbol
     """
     print("load_images")
@@ -117,12 +119,10 @@ def load_images() -> List[Tuple[str, List[int]]]:
     images_list: List[List[int]]
     filepath: str = ""
     for i, character in enumerate(CHARACTER_LIST):
-        print("  load_images -> number ", i, " character ", character)
         filepath = FILEPATH_DATASETS + '/' + character + '.json'  # datatsets/0.json
         images_list = load_image(filepath)  # load list of images of filepath
         for element in images_list:
             if(element):  # list not empty
-                print("    load_images -> adding ", character, element)
                 training_material.append((character, element))
 
     return training_material
@@ -135,21 +135,14 @@ def load_image(filepath: str = '') -> List[List[int]]:
     filepath -- place to look for the file
     """
     print("load_image")
-    drawings_data: List[List[int]]
+    drawings_data: List[List[int]] = []
     if(not os.path.exists(filepath)):
         print('error load_image, file does not exist')
         return [[]]
-    with open(filepath, 'r') as f:
-        json_drawings = json.load(f)
-        drawings_data = json_drawings
-        f.close()  # normally useless but too much bug recently so...
-
-    print(" -> toutes les données chargés du charactère de ", filepath)
-    print(" -> liste de liste ", drawings_data)
     return drawings_data
 
 
-def unload_image(matrix: List[List[int]], character: str = '0') -> None:
+def unload_image(drawing: List[int], character: str = '0') -> None:
     """ Unload image to a json file
 
     Params
@@ -157,26 +150,23 @@ def unload_image(matrix: List[List[int]], character: str = '0') -> None:
     character --
     """
     print("unload_image")
-    filepath = FILEPATH_DATASETS + '/' + character + '.json'
+    filepath = FILEPATH_DATASETS + '/' + 'datasets.csv'
+    data: List[str] = []
     if(not os.path.exists(FILEPATH_DATASETS)):
         print("Dossier n'existe pas")
         os.mkdir(FILEPATH_DATASETS)
     if(not os.path.exists(filepath)):
         print("Fichier n'existe pas")
-        with open(filepath, mode='w+') as f:
-            json.dump([], f)
-            f.close()
+        with open(filepath, mode='w+', encoding='UTF-8') as f:
+            writer = csv.writer(f)
+            writer.writerow(HEADER_ROW)
 
-    print_matrix(character, matrix)
-    new_list = matrix_2_list(matrix)
-    print_list(character, new_list)
-    jsonImages = json.dumps(new_list)
-    with open(filepath, 'r+') as new_file:
-        previousData = json.load(new_file)
-        previousData.append(jsonImages)
-        new_file.seek(0)
-        json.dump(previousData, new_file)
-        new_file.close()
+    with open(filepath, 'a', encoding='UTF-8', newline='')as f:
+        writer = csv.writer(f)
+        drawing_str = list(map(str, drawing))
+        data = [character] + drawing_str
+        print("unload_image -> adding data:\n", data)
+        writer.writerow(data)
 
 
 def delete_model() -> bool:
@@ -190,6 +180,7 @@ def delete_model() -> bool:
         print("  -> delete_model : file already deleted")
         return False
     shutil.rmtree(FILEPATH_MODELS)
+    return True
 
 
 def delete_datasets() -> bool:
@@ -203,3 +194,4 @@ def delete_datasets() -> bool:
         print("  -> delete_dataset : file already deleted")
         return False
     shutil.rmtree(FILEPATH_DATASETS)
+    return True
